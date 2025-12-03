297a2667858d9e9d9159bc35bccfb42f7f22767c
LABORATORIO FELIPE SEGOVIA <laboratorio.fs@ad.uisek.edu.ec>
2025-12-03 10:55:52 -0500

    Laboratorio 7 Finalizado

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,44 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
 import './Tab1.css';
+import RepoItem from '../components/RepoItem';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Repositorios</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Repositorios</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonList>
+          <RepoItem 
+            name="android-project"
+            imageurl="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Android_robot.svg/1022px-Android_robot.svg.png" 
+            /> 
+            
+          <RepoItem 
+            name="iOS-project"
+            imageurl="https://e7.pngegg.com/pngimages/980/413/png-clipart-apple-logo-business-iphone-apple-heart-computer.png" 
+            /> 
+            
+          <RepoItem 
+            name="Ionic-project"
+            imageurl="https://e7.pngegg.com/pngimages/426/603/png-clipart-ionic-new-logo-tech-companies.png" 
+            />
+
+
+
+
+        </IonList>
+
+
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -7,19 +8,27 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil de Usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil de Usuario</IonTitle>
           </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
-      </IonContent>
-    </IonPage>
-  );
+          </IonHeader>
+          <IonCard>
+            <img alt="Silhouette of mountains" src="https://pics.filmaffinity.com/077111620186735-nm_200.jpg" />
+            <IonCardHeader>
+              <IonCardTitle>José Escobar</IonCardTitle>
+              <IonCardSubtitle>josescobar</IonCardSubtitle>
+            </IonCardHeader>
+
+            <IonCardContent>Breve Descripción.</IonCardContent>
+          </IonCard>
+          </IonContent>
+        </IonPage>
+        );
 };
 
-export default Tab3;
+        export default Tab3;
